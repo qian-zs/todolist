41a817fce1a803ce323edbb66123f42f3fbd938f
sheng <sleep_qzs@icloud.com>
2022-04-06 22:00:45 +0800

    组件之间传递数据

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
+  const [title, setTitle] = useState('这是一个标题测试');
+
+  const changeTitle = (title) => {
+    // setTitle('这是第二个标题测试');
+    setTitle(title);
+  }
+
   return (
     <div className="App">
-
+      <Test title={title} changeTitle={changeTitle} />
     </div>
   );
 }

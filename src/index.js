21acfe00e37711770bb37fb790688de0a5fedc37
sheng <sleep_qzs@icloud.com>
2022-04-06 21:49:16 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function

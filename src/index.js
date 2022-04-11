2a9f333f25bf0eae36849127aecc38a88a818d18
sheng <sleep_qzs@icloud.com>
2022-04-11 22:24:32 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/resets.css';
+import './assets/css/border.css';
+import './assets/css/ui.css';
+import './assets/js/common';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 

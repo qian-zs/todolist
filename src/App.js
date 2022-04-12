74b34f274ae50a349d3c76ad3cbfb9df4a546ad8
sheng <sleep_qzs@icloud.com>
2022-04-12 14:47:34 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 import MyHeader from './components/Header';
 import AddInput from './components/AddInput';
 import TodoItem from './components/TodoItem';
+import CheckModal from './components/Modal/CheckModal';
 
 function App() {
   const [isInputShow, setInputShow] = useState(false);
   const [todoList, setTodoList] = useState([]);
+  const [isShowCheckModal, setShowCheckModal] = useState(false);
+  const [currentData, setCurrentData] = useState({});
 
   useEffect(() => {
     const todoData = JSON.parse(localStorage.getItem('todoData') || '[]');
@@ -29,15 +32,21 @@ function App() {
     setInputShow(false);
   }, []);
 
+  const openCheckModal = useCallback((id) => {
+    setCurrentData(() => todoList.filter(item => item.id === id)[0]);
+    setShowCheckModal(true);
+  }, [todoList]);
+
   return (
     <div className="App">
+      <CheckModal isShowCheckModal={isShowCheckModal} closeModal={() => { setShowCheckModal(false) }} data={currentData} />
       <MyHeader openInput={() => setInputShow(!isInputShow)} />
       <AddInput isInputShow={isInputShow} addItem={addItem} />
       <ul className='todo-list'>
         {
           todoList.map((item, index) => {
             return (
-              <TodoItem data={item} key={index} />
+              <TodoItem data={item} key={index} openCheckModal={openCheckModal} />
             )
           })
         }
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,22 +2,22 @@ import React from "react";
 import './index.scss';
 
 function TodoItem(props) {
-    const { data } = props;
-    return (
-        <li className="todo-item">
-            <div className="check-box">
-                <input type="checkbox" checked={data.completed} />
-            </div>
-            <span className="content" style={{ 'textDecoration': data.completed ? 'line-through' : 'none' }}>
-                {data.content}
-            </span>
-            <div className="btn-group">
-                <button className="btn btn-primary">查看</button>
-                <button className="btn btn-warning">编辑</button>
-                <button className="btn btn-danger">删除</button>
-            </div>
-        </li>
-    );
+  const { data, openCheckModal } = props;
+  return (
+    <li className="todo-item">
+      <div className="check-box">
+        <input type="checkbox" defaultChecked={data.completed} />
+      </div>
+      <span className="content" style={{ 'textDecoration': data.completed ? 'line-through' : 'none' }}>
+        {data.content}
+      </span>
+      <div className="btn-group">
+        <button className="btn btn-primary" onClick={() => { openCheckModal(data.id) }}>查看</button>
+        <button className="btn btn-warning">编辑</button>
+        <button className="btn btn-danger">删除</button>
+      </div>
+    </li>
+  );
 }
 
 export default TodoItem;

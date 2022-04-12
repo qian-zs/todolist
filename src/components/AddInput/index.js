733ad43f7a8c7f93c04f8defb6cad88667ac02fb
sheng <sleep_qzs@icloud.com>
2022-04-12 09:17:32 +0800

    commit

--- src/components/AddInput/index.js
+++ src/components/AddInput/index.js
@@ -8,12 +8,12 @@ function AddInput(props) {
     return (
         <>
             {
-                isInputShow ? (
+                isInputShow && (
                     <div className="input-wrapper">
                         <input type="text" placeholder="请输入待办事件" />
                         <button className="btn btn-primary">增加</button>
                     </div>
-                ) : ''
+                )
             }
         </>
     );

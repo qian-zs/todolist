32081958f1b0c43187e43e7f9336024cd6328ae9
sheng <sleep_qzs@icloud.com>
2022-04-12 09:48:55 +0800

    commit

--- src/components/AddInput/index.js
+++ src/components/AddInput/index.js
@@ -2,31 +2,29 @@ import React, { useRef, useState } from "react";
 import './index.scss';
 
 function AddInput(props) {
+  const [inputValue, setInputValue] = useState('');
+  const { isInputShow, addItem } = props;
 
-    const [inputValue, setInputValue] = useState('');
+  const submitValue = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    addItem(inputValue);
+    setInputValue('');
+  };
 
-    const { isInputShow, addItem } = props;
-
-    const submitValue = () => {
-        if (inputValue.length === 0) {
-            return;
-        }
-        addItem(inputValue);
-        setInputValue('');
-    };
-
-    return (
-        <>
-            {
-                isInputShow && (
-                    <div className="input-wrapper">
-                        <input type="text" placeholder="请输入待办事件" onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
-                        <button className="btn btn-primary" onClick={submitValue}>增加</button>
-                    </div>
-                )
-            }
-        </>
-    );
+  return (
+    <>
+      {
+        isInputShow && (
+          <div className="input-wrapper">
+            <input type="text" placeholder="请输入待办事件" onChange={(e) => setInputValue(e.target.value)} value={inputValue} />
+            <button className="btn btn-primary" onClick={submitValue}>增加</button>
+          </div>
+        )
+      }
+    </>
+  );
 }
 
 export default AddInput;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,14 @@ import React from "react";
 import './index.scss';
 
 function Header(props) {
-    const { openInput } = props;
+  const { openInput } = props;
 
-    return (
-        <div className="header">
-            <h1>事件待办</h1>
-            <span className="icon" onClick={openInput}>&#43;</span>
-        </div>
-    );
+  return (
+    <div className="header">
+      <h1>事件待办</h1>
+      <span className="icon" onClick={openInput}>&#43;</span>
+    </div>
+  );
 }
 
 export default Header;

728d54b9abad2c7b1c98253c56fa91cf51e9f8b9
sheng <sleep_qzs@icloud.com>
2022-04-14 10:47:38 +0800

    修改e.target.checked

--- src/components/Modal/EditModal/index.js
+++ src/components/Modal/EditModal/index.js
@@ -34,7 +34,7 @@ function EditModal(props) {
       <p className="topic">
         <textarea className="text-area" ref={inputRef} defaultValue={data.content}></textarea>
       </p>
-      <p className="topic">状态：<input type="checkbox" defaultChecked={data.completed ? true : false} onChange={(e) => setCheckboxValue(e.target.value)} value={checkboxValue} /></p>
+      <p className="topic">状态：<input type="checkbox" defaultChecked={data.completed ? true : false} onChange={(e) => setCheckboxValue(e.target.checked)} value={checkboxValue} /></p>
       <button className="btn btn-primary comfirm-btn" onClick={formatNewData}>提交</button>
     </Modal>
   );
